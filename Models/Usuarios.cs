e91c7173ed79b55c3e85b35cd1614b9a58f329c4
Jhonny1993F <jhonny.montero@udla.edu.ec>
2024-10-03 14:22:22 -0500

    cambios

---
+++ Models/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+        public string? Usuario { get; set; }
+        [PasswordPropertyText]
+        public string? contraseña { get; set; }
+    }
+}
